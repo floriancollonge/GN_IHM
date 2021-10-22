375817fff053b6ab555c2b3951b8d2cb06c403fd
Florian Collonge <fcollonge@greenitpanda.fr>
2021-10-22 16:27:03 +0200

    Adding Tailwind to the project

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from "./router"
 import "@fortawesome/fontawesome-free/css/all.css"
+import './index.css'
 
 createApp(App)
     .use(router)
